9e5ce07ad7cf8e0e8a6734c8c8f35c936a0f5d9c
basalamahalam <101177095+basalamahalam@users.noreply.github.com>
2023-08-23 16:34:15 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Muhammad Alam Basalamah</title>
+    <link rel="stylesheet" href="gaya.css" />
+  </head>
+  <body>
+    <header class="header">
+      <a href="#" class="name">Basalamah Alam.</a>
+      <a href="#" class="name">BA.</a>
+    </header>
+
+    <section class="intro">
+      <img src="pict/alam.png" alt="gaada foto" class="pict" />
+      <div class="intro-content">
+        <h1>Muhammad Alam Basallamah</h1>
+        <h3>Universitas Pendidikan Indonesia</h3>
+        <h5>Ilmu Komputer 2021</h5>
+        <div class="intro-icon">
+          <img src="pict/upi.png" alt="LOGO UPI" />
+          <img src="pict/kemakom.png" alt="LOGO KEMAKOM" />
+        </div>
+      </div>
+      <span class="tambahan"></span>
+    </section>
+  </body>
+</html>
